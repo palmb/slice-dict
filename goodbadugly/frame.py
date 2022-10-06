db8fc5616ae72cd967b3d6110b2c0d41cf3e09af
Bert Palm <bert.palm@ufz.de>
2022-10-06 11:15:35 +0200

    added/improved __str__ and to_string

--- goodbadugly/frame.py
+++ goodbadugly/frame.py
@@ -129,43 +129,83 @@ class Frame:
             raise TypeError(f"{type(value).__name__!r}")
 
     def __str__(self):
+        return self.to_string(max_rows=30, min_rows=10)
+
+    def to_string(self, max_rows=None, min_rows=None, show_df_column_names=None):
+        """
+        Render a Frame to a console-friendly tabular output.
+
+        Parameters
+        ----------
+        max_rows : int, optional
+            Maximum number of rows to display in the console.
+
+        min_rows : int, optional
+            The number of rows to display in the console in a
+            truncated repr (when number of rows is above max_rows).
+
+        show_df_column_names : bool, default None
+            Prints the name of dataframe columns. If `None` the
+            space reserved for column names is removed if
+            not necessary (no Dataframes present).
+
+        Returns
+        -------
+        str
+            Returns the result as a string.
+        """
+        to_pandas_kws = dict(max_rows=max_rows, min_rows=min_rows)
+
         if self.empty:
             return f"Empty {self._name}\nColumns: {self.columns.tolist()}"
-        sep = " | "
-        kws = dict(max_rows=30, min_rows=10)
-        strings = {}
-        for key, val in self._data.items():
-            if hasattr(val, "to_string"):
-                lines = val.to_string(**kws).splitlines()
-            else:  # NA - empty column
-                lines = ["  "]
-            strings[key] = lines
-        header = ""
-        vertline = ""
-        for key, lines in strings.items():
-            key = str(key)
-            header += key.rjust(len(lines[0])) + sep
-            vertline += "=" * max(len(lines[0]), len(key)) + sep
 
-        final = "\n".join([header, vertline]) + "\n"
-        i = 0
+        col_sep, head_sep = " | ", "="
+        iterators = []
+
+        def iter4ever(lines, key: str):
+            l = 0
+            for i, line in enumerate(lines):
+                l = len(line)
+                if i == 0:
+                    yield False, key.rjust(l)
+                    yield False, head_sep * l
+                    if not show_df_column_names:
+                        continue
+                yield False, line
+            while True:
+                yield True, " " * l
+
+        df_is_present = False
+        for key, val in self.data.items():
+            if val.empty:
+                val = pd.Series("", index=[""])
+
+            if isinstance(val, pd.Series):
+                val = val.to_frame(name=" "*len(str(key)))
+            else:
+                df_is_present = True
+
+            lines = val.to_string(**to_pandas_kws).splitlines()
+            iterators.append(iter4ever(lines, str(key)))
+
+        # If the user did not set `show_df_column_names` manually,
+        # we show column names only if a dataframe is present
+        if show_df_column_names is None:
+            show_df_column_names = df_is_present
+
+        string = ""
         while True:
+            exhausted = 0
             line = ""
-            lines_left = len(strings.keys())
-            for key, lines in strings.items():
-                n = max(len(lines[0]), len(str(key)))
-                if i >= len(lines):
-                    part = " "
-                    lines_left -= 1
-                else:
-                    part = lines[i]
-                line += part.rjust(n) + sep
-            if not lines_left:
+            for iterator in iterators:
+                i, s = next(iterator)
+                line += s + col_sep
+                exhausted += i
+            if exhausted == len(iterators):
                 break
-            final += line + "\n"
-            i += 1
+            string += f"{line}\n"
 
-        return final
+        return string
 
     def __repr__(self):
         return str(self)
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import dios
+from goodbadugly import Frame
+
+if __name__ == '__main__':
+    f = Frame(
+        a=pd.Series(range(2)),
+        aVeryLongNameThatIsNowFine=pd.Series(range(4)),
+        # b=pd.DataFrame(columns=list('abc'), index=range(7)),
+        x=pd.Series(dtype=float)
+    )
+    pd.DataFrame.index
+    print(f)
